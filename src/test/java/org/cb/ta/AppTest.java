0953c593633622587f2f355f22689ee5d039da24
orhan <orhanu2020@gmail.com>
2020-12-12 11:54:26 -0500

    basic testng project

--- src/test/java/org/cb/ta/AppTest.java
+++ src/test/java/org/cb/ta/AppTest.java
@@ -1,20 +1,16 @@
 package org.cb.ta;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void gotoCodingBook() {
+        System.setProperty("webdriver.chrome.driver", "webdrivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.codingbook.org");
     }
 }
